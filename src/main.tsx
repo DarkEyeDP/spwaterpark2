2d1cf2b4345138bc66780dc4e37f60c58d249c74
DarkEyeDP <166624576+DarkEyeDP@users.noreply.github.com>
2026-05-29 16:19:52 -0500

    chore: update version to 1.4.6 and integrate Microsoft Clarity for enhanced analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,10 @@
 
   import { createRoot } from "react-dom/client";
+  import Clarity from "@microsoft/clarity";
   import App from "./app/App.tsx";
   import "./styles/index.css";
+
+  Clarity.init("wyve4loc5r");
 
   createRoot(document.getElementById("root")!).render(<App />);
   
